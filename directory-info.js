87f90af0dedf709f6ea9b8bf55ac992155d38bc1
Dmitry <dimonkozlovskiy@gmail.com>
2017-09-10 01:57:13 +0300

    fix main script

--- directory-info.js
+++ directory-info.js
@@ -3,3 +3,18 @@ const path = require('path');
 
 const directoryPath = process.argv[2];
 
+if (directoryPath !== undefined) {
+    fs.stat(directoryPath, (err, stats) => {
+        if (err || !(stats.isDirectory())) {
+            console.error("Неверный путь!");
+        } else {
+            fs.readFile("summary-template.js", "utf8", (error, data) => {
+                let scriptData = data.replace("{directoryPath}", directoryPath);
+                fs.writeFile(directoryPath + '/' + 'summary.js', scriptData);
+            });
+            console.log('node ' + directoryPath + '\\summary.js');
+        }
+    });
+} else {
+    console.error("Введите путь к файлу!");
+}
