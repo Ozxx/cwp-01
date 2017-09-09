ed36838de74d6744b1d021a45096a5afd1509771
Dmitry <dimonkozlovskiy@gmail.com>
2017-09-10 01:14:12 +0300

    add copy txt file function

--- summary-template.js
+++ summary-template.js
@@ -1,15 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-const directoryPath = process.argv[2];
-//const directoryPath = '{directoryPath}';
+const directoryPath = '{directoryPath}';
 
 function getDirectoryInfo(dirPath) {
     fs.readdir(dirPath, (error, data) => {
-        data.forEach((file) =>
-        {
-            fs.stat(dirPath + '/' + file, (error, state) =>
-            {
+        data.forEach((file) => {
+            fs.stat(dirPath + '/' + file, (error, state) => {
                 const node = dirPath + '/' + file;
                 if (state.isDirectory()) {
                     getDirectoryInfo(node);
@@ -24,17 +21,67 @@ function getDirectoryInfo(dirPath) {
 function createBaseDirectory(dirPath) {
     const baseDirectory = path.parse(dirPath)['base'];
     const baseDirectoryPath = dirPath + '/' + baseDirectory;
-    fs.mkdir(baseDirectoryPath, (error) =>
-    {
+    fs.mkdir(baseDirectoryPath, (error) => {
         if (error.code === 'EEXIST') {
             console.log(
                 '***Notification: ' + baseDirectory +
                 'already exists on' + baseDirectoryPath);
         }
     });
-    console.log(baseDirectory);
+    return baseDirectoryPath;
+}
+
+function getCopyright() {
+    let copyright = "";
+    fs.readFile('config.json', function (err, data) {
+        if (err) {
+            throw new Error('Copyright not specified');
+        } else {
+            copyright = JSON.parse(data.toString());
+        }
+    });
+    return copyright;
+}
+
+function copyTxtFiles(from, to) {
+    const baseName = path.parse(to)['base'];
+    const copyright = getCopyright();
+    fs.readdir(from, (error, data) => {
+        data.forEach((file) => {
+            fs.stat(from + '/' + file, (error, state) => {
+                const node = from + '/' + file;
+                if (file !== baseName) {
+                    if (state.isDirectory()) {
+                        copyTxtFiles(node, to);
+                    } else {
+                        if (path.extname(file).toLowerCase() === '.txt') {
+                            fs.createReadStream(from + '/' +file).pipe(fs.createWriteStream(to + '/' + file));
+                            fs.readFile(to + '/' + file, (err, data) => {
+                                if (err) {
+                                    console.error('');
+                                } else {
+                                    newData = copyright + data.toString() + copyright;
+                                    fs.writeFile(to + '/' + file, newData, 'utf8', () => {});
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+        })
+    });
+}
+
+try {
+    getDirectoryInfo(directoryPath);
+    let base = createBaseDirectory(directoryPath);
+    copyTxtFiles(directoryPath, base);
+    fs.watch(base, {encoding: 'buffer'}, (eventType, filename) => {
+    if (filename) { console.log(filename.toString()); }
+    });
+} catch (error) {
+    console.log(error.message);
 }
 
 
-getDirectoryInfo(directoryPath);
-createBaseDirectory(directoryPath);
+
